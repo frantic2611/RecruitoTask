c4ef8b1a246a5b5e9cd522add960b71a4788613c
Frantic <sagittarius.00@mail.ru>
2024-12-03 16:24:23 +0300

    commit 1.2 (added pages and form)

--- main/views.py
+++ main/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, 'home.html')
+
+def greeting(request):
+    name = request.GET.get('name')
+    message = request.GET.get('message')
+    return render(request, 'greeting.html', {'name':name, 'message':message})
